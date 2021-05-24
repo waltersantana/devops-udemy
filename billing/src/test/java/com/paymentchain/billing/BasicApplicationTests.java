02fbf993756f19792ea6c5c4c35673741fb72206
Walter Santana <walterrafaelsantana@gmail.com>
2021-05-24 09:32:26 -0300

    Corrección por falta de imports 2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -3,7 +3,7 @@ package com.paymentchain.billing;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.postgresql.translation.message_bg;
+import org.postgresql.translation.messages_bg;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
